9e7edb5a7cb5294f04f618282533dc54dacc14f1
Henry Tran <henry.tran@shopify.com>
2018-07-30 15:55:47 -0400

    Refactor EWMA to not use shared dictionaries
    (cherry picked from commit 3acdeece33ceacfdba4efb9e20940253f2a39db1)

--- internal/ingress/controller/template/template.go
+++ internal/ingress/controller/template/template.go
@@ -195,8 +195,6 @@ func buildLuaSharedDictionaries(s interface{}, dynamicConfigurationEnabled bool,
 			"lua_shared_dict configuration_data 5M",
 			"lua_shared_dict certificate_data 16M",
 			"lua_shared_dict locks 512k",
-			"lua_shared_dict balancer_ewma 1M",
-			"lua_shared_dict balancer_ewma_last_touched_at 1M",
 			"lua_shared_dict sticky_sessions 1M",
 		)
 	}
